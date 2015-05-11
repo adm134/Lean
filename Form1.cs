59e2fbf585922c38500e62775581a3f2fc3fea68
adm1341 <adm134@mail.ru>
2015-05-11 14:23:10 +0500

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+
 using System.Text;
 using System.Windows.Forms;
 using System.IO;
 using System.Security.Cryptography;
 namespace EncryptFile
-{gdfgdfgdfg
+{gdfgdfgdfg 
+tttttt
     public partial class Form1 : Form
     {
         public Form1()
